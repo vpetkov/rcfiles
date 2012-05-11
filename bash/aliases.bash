25c02662981c969b049ec0ba882028a90b38e64c
Vesselin Petkov <vpetkov@mm-sol.com>
2012-05-11 16:32:54 +0300

    bash: aliases: change double quotes with single quotes

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-alias realias="source ~/.bash/aliases.bash"
-alias aliases="vi ~/.bash/aliases.bash && realias"
+alias realias='source ~/.bash/aliases.bash'
+alias aliases='vi ~/.bash/aliases.bash && realias'
 
 # some more ls aliases
 alias ll='ls -lhGF'
@@ -8,49 +8,49 @@ alias la='ls -lhGFA'
 alias l='ls -1F'
 
 # open vertical vim bufers
-alias vi=vim
+alias vi='vim'
 alias vim='vim -O'
 
 # update board's clock
 alias update_marmaduke_time='ssh root@marmaduke "date -s \"$(date -u)\""'
 
 # rhythmbox
-alias music="rhythmbox-client --play-pause"
-alias song="rhythmbox-client --print-playing-format \"%tt by %ta from %at (%te/%td)\""
-alias previous="rhythmbox-client --previous"
-alias next="rhythmbox-client --next"
+alias music='rhythmbox-client --play-pause'
+alias song='rhythmbox-client --print-playing-format \"%tt by %ta from %at (%te/%td)\"'
+alias previous='rhythmbox-client --previous'
+alias next='rhythmbox-client --next'
 
 # python
-alias py="python -ic \"import readline, rlcompleter; readline.parse_and_bind('tab: complete'); del readline, rlcompleter\""
+alias py='python -ic \"import readline, rlcompleter; readline.parse_and_bind("tab: complete"); del readline, rlcompleter\"'
 
 # picocom
-alias pcom="picocom -b 115200 -l"
+alias pcom='picocom -b 115200 -l'
 
 # lock screen
-alias lock="gnome-screensaver-command --lock"
+alias lock='gnome-screensaver-command --lock'
 
 #adb
-alias adb="sudo ~/code/android_tools/adb"
+alias adb='sudo ~/code/android_tools/adb'
 
 #fastboot
-alias fastboot="sudo ~/code/android_tools/fastboot"
-alias usbboot="sudo ~/code/android_tools/usbboot ~/code/android_tools/bootloader.bin"
+alias fastboot='sudo ~/code/android_tools/fastboot'
+alias usbboot='sudo ~/code/android_tools/usbboot ~/code/android_tools/bootloader.bin'
 
 #mutt
-alias ti="mutt -F ~/.muttrc.ti"
-alias mms="mutt -F ~/.muttrc.mms"
+alias ti='mutt -F ~/.muttrc.ti'
+alias mms='mutt -F ~/.muttrc.mms'
 
 #week number
-alias week="date +%W"
-alias day="date +%a | tr \"[:upper:]\" \"[:lower:]\""
+alias week='date +%W'
+alias day='date +%a | tr \"[:upper:]\" \"[:lower:]\"'
 
 #repo sync ReTry
 alias repo-sync='time until repo sync; do echo "FAILED: trying again"; done'
 
 #git
-alias g="git"
-alias gu="git update"
-alias gg="git status"
+alias g='git'
+alias gu='git update'
+alias gg='git status'
 alias glg='git lg'
 alias gla='git la'
 alias gl='git ls'
