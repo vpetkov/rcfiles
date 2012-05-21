cefdebf6beffdb38a0e099cd27972913c2c9b2f8
Vesselin Petkov <mail@vpetkov.com>
2012-05-21 12:48:08 +0300

    bash: remove platform specific paths, rename bash_profile -> bashrc

--- bash/paths.bash
+++ bash/paths.bash
@@ -1,9 +1,4 @@
-PATH="$PATH:$HOME/bin"
+PATH="$HOME/bin:$PATH"
+PATH="$PATH:$HOME/arm-2010q1/bin"
 
-if echo $my_uname | grep -q "Linux"; then
-    PATH="$PATH:$HOME/arm"
-elif echo $my_uname | grep -q "Drawin"; then
-    PATH="$PATH:/usr/local/Cellar/bash/4.2.28/bin/bash"
-fi
-
-export PATH
+export PATH=$PATH
