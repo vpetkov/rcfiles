aae89fef5a6dfc82bdfb0d41a759875ee3747d85
Vesselin Petkov <mail@vpetkov.com>
2012-05-25 18:54:20 +0300

    bash: add os x specific configs

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -6,6 +6,7 @@ alias aliases='vi ~/.bash/aliases.bash && realias'
 alias ls='ls -GF'
 alias ll='ls -lh'
 alias la='ll -A'
+alias ld='ll | grep "^d"'
 alias l='ls -1'
 
 # open vertical vim bufers
@@ -18,21 +19,32 @@ alias py='ipython'
 # picocom
 alias pcom='picocom -b 115200 -l'
 
-#fastboot
+# fastboot
 alias fastboot='sudo ~/code/android_tools/fastboot'
 alias usbboot='sudo ~/code/android_tools/usbboot ~/code/android_tools/bootloader.bin'
 
-#week number
+# week number
 alias week='date +%W'
 alias day='date +%a | tr \"[:upper:]\" \"[:lower:]\"'
 
-#repo sync ReTry
+# repo sync re-try
 alias repo-sync='time until repo sync; do echo "FAILED: trying again"; done'
 
-#git
+# git
 alias gu='git update'
 alias gg='git status'
 alias glg='git lg'
 alias gla='git la'
 alias gl='git ls'
+
+if [[ "$OSTYPE" =~ ^darwin ]]
+then
+    # get OS X software updates
+    # update Homebrew itself, and upgrade installed Homebrew packages
+    alias update='sudo softwareupdate -i -a; brew update; brew upgrade'
+
+    # show/hide Desktop icons
+    alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+    alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+fi
 
