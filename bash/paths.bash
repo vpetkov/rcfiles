b407e7ab2e5e9d27223b553160492f67d11c76e8
Vesselin Petkov <vpetkov@me.com>
2013-01-29 14:19:19 +0100

    gem path
    Signed-off-by: Vesselin Petkov <vpetkov@me.com>

--- bash/paths.bash
+++ bash/paths.bash
@@ -2,4 +2,9 @@ PATH="$HOME/bin:$PATH"
 PATH=$PATH:$HOME/.rvm/bin
 PATH="/usr/local/bin:$PATH"
 
+GEM_PATH=~/.gem
+GEM_HOME=$GEM_PATH
+PATH="$PATH:$GEM_PATH/bin"
+
+export RBENV_ROOT=/usr/local/opt/rbenv
 export PATH=$PATH
--- bashrc
+++ bashrc
@@ -46,4 +46,3 @@ complete -o "nospace" -W "Finder Dock Mail Safari iTunes iCal Address\ Book Syst
 
 # rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-export RBENV_ROOT=/usr/local/opt/rbenv
